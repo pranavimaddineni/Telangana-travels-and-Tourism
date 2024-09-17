009f606c55bdf95bebca7cc74949a3e4a3370cb4
pranavimaddineni <161917107+pranavimaddineni@users.noreply.github.com>
2024-09-17 09:10:27 +0530

    Telangana Travels &Tourism

---
+++ RTSR PROJECT (2)/script.js
@@ -0,0 +1,15 @@
+const showPopupBtn=document.querySelector(".login-btn");
+const formPopup=document.querySelector(".form-popup");
+const hidePopupBtn=document.querySelector(".form-popup .close-btn");
+const loginSignupLink=document.querySelectorAll(".form-box .bottom-link a");
+showPopupBtn.addEventListener("click",()=>{
+    document.body.classList.toggle("show-popup");
+});
+hidePopupBtn.addEventListener("click",()=>showPopupBtn.click());
+loginSignupLink.forEach(link => {
+    link.addEventListener("click",(e)=>{
+       e.preventDefault();
+       formPopup.classList[link.id ==="signup-link" ? 'add':'remove']("show-signup");
+   
+    });
+});
